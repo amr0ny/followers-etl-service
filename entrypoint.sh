9a05652ce9b124d9a81c3e739a0291160b0a30d0
Alex <38862440+amr0ny@users.noreply.github.com>
2025-07-03 00:19:51 +0300

    fix: unnecessary file removed

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-echo "Running database migrations..."
-migrate -path ./migrations -database "${DSN}" up || { echo "Migration failed"; exit 1; }
-
-echo "Starting application..."
-exec "$@"
